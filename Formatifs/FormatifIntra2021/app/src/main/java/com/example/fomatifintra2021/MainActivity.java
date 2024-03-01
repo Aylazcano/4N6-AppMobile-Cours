7973ea802b4671ead06c76a2c312fe9e80203a89
Alexis Lazcano <0370102@cegepmontpetit.ca>
2024-03-01 15:08:44 -0500

    PROGRES

---
+++ Formatifs/FormatifIntra2021/app/src/main/java/com/example/fomatifintra2021/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.fomatifintra2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Activiy1");
+    }
+}
